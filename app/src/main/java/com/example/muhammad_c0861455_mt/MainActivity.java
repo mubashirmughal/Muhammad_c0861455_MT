8c6bb4fd58153186538260543c649b9d632a771f
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-12-05 19:03:42 -0500

    initial commit

---
+++ app/src/main/java/com/example/muhammad_c0861455_mt/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.muhammad_c0861455_mt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
